af7571218a1f6f34bfab8872b26eeeea96077476
Zeeshan <zeeshankhan@aerchain.io>
2021-02-27 02:12:49 +0530

    added redux and password based authentication

--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import rootReducer from "./reducers";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import "antd/dist/antd.css";
 
+const store = createStore(rootReducer, composeWithDevTools());
+
 ReactDOM.render(
   // <React.StrictMode>
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   // </React.StrictMode>,
   document.getElementById("root")
 );
