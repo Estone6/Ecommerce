d51264fa6eae3652dc755a2b74378bff01b6c032
Zeeshan <zeeshankhan@aerchain.io>
2021-02-28 16:29:32 +0530

    completed firebase authentication steps - login, register, forgot password

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -1,7 +1,127 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Button } from "antd";
+import { MailOutlined, GoogleOutlined } from "@ant-design/icons";
+import { auth, googleAuthProvider } from "../../firebase";
+import { toast } from "react-toastify";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
-const Login = () => {
-  return <div>Login</div>;
+const Login = ({ history }) => {
+  const [email, setEmail] = useState("er.zeeshankhan6@gmail.com");
+  const [password, setPassword] = useState("123456");
+  const [loading, setLoading] = useState(false);
+  const dispatch = useDispatch();
+
+  const { user } = useSelector((state) => ({ ...state }));
+
+  useEffect(() => {
+    if (user && user.token) history.push("/");
+  }, [user]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    try {
+      const result = await auth.signInWithEmailAndPassword(email, password);
+      const { user } = result;
+      const idTokenResult = await user.getIdTokenResult();
+      dispatch({
+        type: "LOGGED_IN_USER",
+        payload: {
+          email: user.email,
+          token: idTokenResult.token,
+        },
+      });
+      history.push("/");
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+      setLoading(false);
+    }
+  };
+  const googleLogin = async () => {
+    auth
+      .signInWithPopup(googleAuthProvider)
+      .then(async (result) => {
+        const { user } = result;
+        const idTokenResult = await user.getIdTokenResult();
+        dispatch({
+          type: "LOGGED_IN_USER",
+          payload: {
+            email: user.email,
+            token: idTokenResult.token,
+          },
+        });
+        history.push("/");
+      })
+      .catch((err) => {
+        console.log(err);
+        toast.error(err.message);
+      });
+  };
+  const loginForm = () => (
+    <form onSubmit={handleSubmit}>
+      <div className="form-group">
+        <input
+          type="email"
+          className="form-control"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          autoFocus
+          placeholder="Enter your email"
+        />
+      </div>
+      <div className="form-group">
+        <input
+          type="password"
+          className="form-control"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Enter your password"
+        />
+      </div>
+      <br />
+      <Button
+        type="primary"
+        onClick={handleSubmit}
+        className="mb-3"
+        block
+        shape="round"
+        icon={<MailOutlined />}
+        size="large"
+        disabled={!email || password.length < 6}
+        loading={loading}
+      >
+        Login with Email/Password
+      </Button>
+    </form>
+  );
+  return (
+    <div className="container p-5">
+      <div className="row">
+        <div className="col-md-6 offset-md-3">
+          {!loading ? <h4>Login</h4> : <h4 className="text-danger">Loading</h4>}
+          {loginForm()}
+
+          <Button
+            type="danger"
+            onClick={googleLogin}
+            className="mb-3"
+            block
+            shape="round"
+            icon={<GoogleOutlined />}
+            size="large"
+            loading={loading}
+          >
+            Login with Google
+          </Button>
+          <Link to="/forgot/password" className="float-right text-danger">
+            Forgot Password
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- src/pages/auth/Register.js
+++ src/pages/auth/Register.js
@@ -1,9 +1,17 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { auth } from "../../firebase";
 import { toast } from "react-toastify";
+import { useSelector } from "react-redux";
 
-const Register = () => {
+const Register = ({ history }) => {
   const [email, setEmail] = useState("");
+
+  const { user } = useSelector((state) => ({ ...state }));
+
+  useEffect(() => {
+    if (user && user.token) history.push("/");
+  }, [user]);
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     const config = {
@@ -19,11 +27,13 @@ const Register = () => {
     <form onSubmit={handleSubmit}>
       <input
         type="email"
-        className="form-control mb-3"
+        className="form-control"
         value={email}
         onChange={(e) => setEmail(e.target.value)}
         autoFocus
+        placeholder="Enter your email"
       />
+      <br />
       <button type="submit" className="btn btn-raised">
         Register
       </button>
